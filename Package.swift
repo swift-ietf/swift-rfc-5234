509c61adeec8cf7c3601de4c496dcbf8d26d3236
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-03 07:30:01 +0200

    Apply mechanical lint fixes (swift-format, SwiftLint)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .macOS(.v26),
         .iOS(.v26),
         .tvOS(.v26),
-        .watchOS(.v26)
+        .watchOS(.v26),
     ],
     products: [
         .library(
@@ -29,23 +29,32 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-incits/swift-incits-4-1986.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-primitives.git",
+            branch: "main"
+        ),
+        .package(url: "https://github.com/swift-incits/swift-incits-4-1986.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "RFC 5234",
             dependencies: [
-                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .product(
+                    name: "Standard Library Extensions",
+                    package: "swift-standard-library-extensions"
+                ),
                 .product(name: "ASCII Primitives", package: "swift-ascii-primitives"),
-                .product(name: "INCITS 4 1986", package: "swift-incits-4-1986")
-    ]
+                .product(name: "INCITS 4 1986", package: "swift-incits-4-1986"),
+            ]
         ),
         .testTarget(
             name: "RFC 5234 Tests",
             dependencies: [
-                "RFC 5234",
+                "RFC 5234"
             ]
         ),
     ],
